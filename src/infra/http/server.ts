cf92664c4b77f072235e43fe04ce6a974adacd68
Alexandre <alexandre.oliveira@atip.io>
2025-01-23 15:02:50 -0500

    Primeiro commit - adicionando upload-server ftr aulas

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,13 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+import { env } from "@/env";
 
 const server = fastify();
 
 server.register(fastifyCors, { origin: "*" })
 
+console.log(env.DATABASE_URL)
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server is running!');
-});
-
-
+})
