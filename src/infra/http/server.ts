ab9c785ed6df64b15b7ac78a77851915b8d3ac99
Alexandre Oliveira <alexandre.yukon@gmail.com>
2025-01-24 08:43:37 -0500

    fix/databaseurl

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,12 @@
-import fastify from "fastify";
-import fastifyCors from "@fastify/cors";
-import { env } from "@/env";
+import { env } from '@/env'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 
-const server = fastify();
+const server = fastify()
 
-server.register(fastifyCors, { origin: "*" })
+server.register(fastifyCors, { origin: '*' })
 
 console.log(env.DATABASE_URL)
-
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server is running!');
+  console.log('HTTP Server running!')
 })
